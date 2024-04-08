77108d595dfbae45d24b6cf514257f828423f74d
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2024-04-08 19:42:34 +0700

    Fix Update API

--- Backend/src/post/post.controller.ts
+++ Backend/src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -38,7 +38,7 @@ export class PostController {
     return this.postService.findOne(+id);
   }
 
-  @Patch('update/:id')
+  @Put('update/:id')
   async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     try {
       return await this.postService.update(+id, updatePostDto);
