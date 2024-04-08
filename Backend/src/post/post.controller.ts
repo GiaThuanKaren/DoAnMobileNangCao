cd25a133bec4c4916294004db18e666fe6caa854
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2024-04-08 19:32:53 +0700

    Fix Update Post

--- Backend/src/post/post.controller.ts
+++ Backend/src/post/post.controller.ts
@@ -39,8 +39,13 @@ export class PostController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    try {
+      return await this.postService.update(+id, updatePostDto);
+
+    } catch (error) {
+      return error
+    }
   }
 
   @Delete(':id')
