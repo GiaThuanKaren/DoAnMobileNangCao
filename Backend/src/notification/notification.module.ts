313326ca425b288cd4f578c7ae004f50b4588327
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2024-05-06 10:40:36 +0700

    remove Notify Server

--- Backend/src/notification/notification.module.ts
+++ Backend/src/notification/notification.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
-import { messagingProvider } from './MessageProvider';
-import { MessagingService } from './MessagingService';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { User } from '../users/entites/user.entity';
